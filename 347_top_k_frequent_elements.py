ca931336e07c80b78248ba3d79723a458bfd6775
Khongorzul Khenchbish <khongorzul.khenchbish@ericsson.com>
2025-12-14 14:42:16 +0100

    hashmap, min_heap

--- 347_top_k_frequent_elements.py
+++ 347_top_k_frequent_elements.py
@@ -2,7 +2,7 @@ import heapq
 
 class Solution:
     def topKFrequent(snumf, nums: List[int], k: int) -> int:
-        # time: O(n) with bucket sort, space will be O(n)
+        # Linear time solution: bucket sort
         # nums = [1,1,1,2,2,100]
         freq = {}
         for num in nums:
@@ -20,9 +20,9 @@ class Solution:
         
         # traverse backwards because the freq is decreasing by index
         kmost = []
-        # time: O(n)
+        # time: O(N), space: O(N)
         for i in range(len(buckets)-1, 0, -1):
-            # although it seems like nested loop, the time will be O(n+n) 
+            # although it seems like nested loop, the time will be O(n+n)
             while buckets[i]:
                 kmost.append(buckets[i].pop())
                 if len(kmost) == k:
@@ -31,22 +31,27 @@ class Solution:
         return []
 
 
-        # Heap solution - O(n*logn)
-        # counter = {}
+        # Heap solution - # Time O(N*logK), Space O(K)
+        counter = {}
 
-        # for num in nums:
-        #     counter[num] = 1 + counter.get(num, 0)
+        for num in nums:
+            counter[num] = 1 + counter.get(num, 0)
 
         # Solution 1: heap
-        # heap = []
-        # for key, val in counter.items():
-        #     if len(heap) < k:
-        #         heapq.heappush(heap, (val, key))
-        #     numse:
-        #         # heap is exactly k
-        #         heapq.heappushpop(heap, (val, key))
+        min_heap = []
+        for key, val in counter.items():
+            if len(min_heap) < k:
+                # insert into the heap to be sorted by first(val) of the tuple
+                heapq.heappush(min_heap, (val, key))
+            else:
+                # heap is exactly k
+                heapq.heappushpop(min_heap, (val, key))
         
-        # return [h[1] for h in heap]
-
-        # Time O(N*logK)
-        # Space O(N)
+        return [h[1] for h in min_heap]
+        """
+        nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+        counter = {"1":4, "2":4, "3":2}
+        min_heap = [(4,1), (4,2), (2,3)] store only k => [(4,1), (4,2)]
+        return first(val) only from the tuples => [1, 2]
+        1 and 2 are the k=2 elements which appeared the most.
+        """
