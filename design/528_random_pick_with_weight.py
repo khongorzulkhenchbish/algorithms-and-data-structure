a2c9b30fd030164cbbcc6c4bb7e165992d3fd361
Khongorzul Khenchbish <lolhongorzul@gmail.com>
2026-02-12 00:08:33 +0100

    prefix sum saves space

--- design/528_random_pick_with_weight.py
+++ design/528_random_pick_with_weight.py
@@ -1,55 +1,45 @@
 class Solution:
+    """ The scale of the input 10^9 because each number can have up to 10^5 repetition
+    Time: O(N) for init, O(logN) for pickIndex
+    Space: O(N), prefix sum, binary search
+
+    Intuition
+    given array:                                   [1, 200,  3,   1,   2]
+    1. assign indexes                              [0,  1,   2,   3,   4]
+    2. prefix sum represents the weight, frequency [0, 201,  204, 205, 207]
     """
-    Time: O(N), Space: O(logN), prefix sum, binary search
-    Intuition: Imagine dividing a cake to people at the party in a way person with the highest weight
-    gets the largest probability of picking the slices.
-
-    Suppose we have people with weight [1,2,3,4,3] and we decided to order cake proportional to our
-    total weight = 1+2+3+4+3 = 13 pound cake.
-
-    This can be represented as [0, 1,1, 2,2,2, 3,3,3,3 4,4,4]. We want to get "slice randomly" and
-    distribute the "human deterministically". Example: Chance of picking 2 slices is 3/13.
-
-    So the slice 0 belongs to 0 pound guy.
-           slice 1-2 belongs to 1 pound guy.
-           slice 3-5 belongs to 2 pound guy.
-           slice 6-9 belongs to 3 pound guy.
-           slice 10-12 belongs to 4 pound guy.
-
-    This pattern brings the prefixSum = [1,1+2,1+2+3, ..] = [1,3,6,10,13]
-    """
-
     def __init__(self, w: List[int]):
-        self.prefix_sums = []
+        self.prefix = []
         total_until = 0
 
         for weight in w:
             total_until += weight
-            self.prefix_sums.append(total_until)
+            self.prefix.append(total_until)
 
         self.total_sum = total_until
 
     def pickIndex(self) -> int:
-        target = self.total_sum * random.random()
+        # uniform will return number between 0, N with equal distribution.
+        target = random.uniform(0, self.total_sum)
+        # target will be between [0, 207] and we have to return the index where the val == target
+        # target can be 105 for example.
 
         # input is sorted, binary search to find which index will be 
-        # returned according to its probability, we search in the extended array
-        # => [0, 1,1, 2,2,2, 3,3,3,3 4,4,4] => [0:1, 1:2, 2:3, 3:4, 4:3] => [1, 3, 6, 10, 13]
-        # searching on prefix_sums=[1, 3, 6, 10, 13] is faster than on extended=[0, 1,1, 2,2,2, 3,3,3,3 4,4,4]
-        left, right = 0, len(self.prefix_sums)
+        # returned according to its "WEIGHT (probability)", we search in the extended array
+        # searching on prefix=[0,201,204,205,207] is faster than on extended=[0, 1,1..(200X), 2,2,2, 3, 4,4]
+        left, right = 0, len(self.prefix)
 
         while left < right:
-            mid = left + (right - left) // 2
-            if target > self.prefix_sums[mid]:
+            mid = (left + right) // 2
+            if target > self.prefix[mid]:
                 left = mid + 1 # go to the right subarray
             else:
                 right = mid # go to the left subarray
-        
+        # target = 105
+        # left = 0, 1
+        # right = 5, 2, 1
+        # mid = 2 => (105 < 204), 1 => (105 < 201), 0 => (105 < 0) not ==> left == right => 1
+        # prefix_array = [0, 201, 204, 205, 207]
+        # 1 being return value means, 105 is between [0,201] and in the og_arr=[1,200,....] 200 has the
+        # most frequence and should be picked the most.
         return left
-
-        
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(w)
-# param_1 = obj.pickIndex()
