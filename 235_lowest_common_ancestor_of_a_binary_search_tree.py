1280a01be214f923256b2fbd3bc87259fa211a49
Khongorzul Khenchbish <lolhongorzul@gmail.com>
2026-02-19 21:10:48 +0100

    binary tree, iterative

--- 235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ 235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -7,29 +7,41 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        ''' Tip: Think about binary tree rule. lower on the left, higher on the right side.
+           6     => Case 1. p=7, q=9 => LCA is in the right subtree
+         2   8   => Case 2. p=2, q=4 => LCA is in the left subtree
+        0 4 7 9  => Case 3. p=2, q=8 => LCA is the current, when p and q are separate sides.
+         3 5
         '''
-        Time: O(log n) only visit one of the level nodes
-        Space: O(log n) only stores halft times the nodes
-        Space: O()
-        Tip: Think about binary tree rule. lower on the left, higher on the right side.
-          6     => Case 1. p=7, q=9 => LCA is in the right subtree
-        2   8   => Case 2. p=2, q=4 => LCA is in the left subtree
-       0 4 7 9  => Case 3. p=2, q=8 => LCA is the current, when p and q are separate sides.
-        3 5
-        '''
+        # Iterative approach
+        # space: O(1) superior on larger input
+        cur = root
         
+        while cur:
+            cv, pv, qv = cur.val, p.val, q.val
+            
+            if cv < pv and cv < qv:
+                cur = cur.right
+            elif cv > pv and cv > qv:
+                cur = cur.left
+            else:
+                return cur
+        
+        # Approach 1: recursive
+        # Time: O(log n) only visit one of the level nodes
+        # Space: O(log n) only stores half times the nodes
         if not root:
             return None
 
         if root == p or root == q:
             return root
-        
+
         if root.val <= p.val and root.val <= q.val: # LCA is on the right side
             return self.lowestCommonAncestor(root.right, p, q)
         
-        if root.val >= p.val and root.val >= q.val: # LCA is on the left side
+        elif root.val >= p.val and root.val >= q.val: # LCA is on the left side
             return self.lowestCommonAncestor(root.left, p, q)
-
-        if root.val >= p.val and root.val <= q.val: # LCA is the root itself
+        
+        else: # root.val >= p.val and root.val <= q.val: # LCA is the root itself
             return root
         
